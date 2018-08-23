af3fa092e9b742f5ba287afbcaffc44de2f3f44f
Ryan Coulson <contact@ryan-coulson.com>
2018-08-23 00:49:54 -0400

    added font awesome support and add new button

--- index.php
+++ index.php
@@ -7,12 +7,16 @@
 
 		<!-- favicon -->
 		<link rel="shortcut icon" type="image/png" href="resources/media/ryan-coulson-logo3.png"/>
+
+		<!-- fort awesome icons -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 	</head>
 	<body>
 
 		<!-- top navigation -->
 		<div class="navigation">
 			<img src="resources/media/ryan-coulson-logo3.png" />
+			<div class="btn"><i class="fa fa-file"></i>Add New</div>
 		</div>
 
 		<!-- side navigation -->
