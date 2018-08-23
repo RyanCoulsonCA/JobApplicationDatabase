9fa5a4e26029ea838839bf1179a8fe6f03c3740a
Ryan Coulson <contact@ryan-coulson.com>
2018-08-23 00:41:48 -0400

    began work on layout

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <html lang="en-US">
+	<head>
+		<title>Job Application Database</title>
 
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="resources/styles/stylesheet.css" />
+
+		<!-- favicon -->
+		<link rel="shortcut icon" type="image/png" href="resources/media/ryan-coulson-logo3.png"/>
+	</head>
+	<body>
+
+		<!-- top navigation -->
+		<div class="navigation">
+			<img src="resources/media/ryan-coulson-logo3.png" />
+		</div>
+
+		<!-- side navigation -->
+		<div class="side-container">
+			<input type="text" name="query" placeholder="Enter Query" autocomplete="off" />
+		</div>
+	</body>
 </html>
